1514b17d09f4276e89416e4e5c0a0e3564c41d1e
rhodrin <rhodri.nelson@gmail.com>
2020-06-09 22:23:00 +0100

    Remove custom compiler.

--- devito/compiler.py
+++ devito/compiler.py
@@ -538,13 +538,14 @@ class CustomCompiler(Compiler):
     or otherwise default to ``-fopenmp``.
     """
 
-    def __new__(cls, *args, **kwargs):
-        if any(i in environ for i in ['CC', 'CXX', 'CFLAGS', 'LDFLAGS']):
-            obj = super().__new__(cls, *args, **kwargs)
-            obj.__init__(*args, **kwargs)
-            return obj
-        else:
-            return GNUCompiler(*args, **kwargs)
+    #def __new__(cls, *args, **kwargs):
+        #if any(i in environ for i in ['CC', 'CXX', 'CFLAGS', 'LDFLAGS']):
+            #obj = super().__new__(cls, *args, **kwargs)
+            #obj.__init__(*args, **kwargs)
+            #return obj
+        #else:
+            #return GNUCompiler(*args, **kwargs)
+        #return GNUCompiler(*args, **kwargs)
 
     def __init__(self, *args, **kwargs):
         super(CustomCompiler, self).__init__(*args, **kwargs)
